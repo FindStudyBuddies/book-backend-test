0f5a194b4d657c05b980979f420e2f32792b65d3
Varun Jindal <vjindal0112@gmail.com>
2021-03-04 17:12:24 -0500

    post request working

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import logging
 import sqlalchemy 
 import os
 
+
 db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
 #cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
 db_user = os.environ.get('CLOUD_SQL_USERNAME')
@@ -12,28 +13,28 @@ db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
 
 def open_connection():
-
-    pool = sqlalchemy.create_engine(
-    
-        # Equivalent URL:
-        # postgres+pg8000://<db_user>:<db_pass>@/<db_name>
-        #                         ?unix_sock=<socket_path>/<cloud_sql_instance_name>/.s.PGSQL.5432
-        sqlalchemy.engine.url.URL(
-            drivername="postgresql+pg8000",
-            username=db_user,  # e.g. "my-database-user"
-            password=db_pass,  # e.g. "my-database-password"
-            database=db_name,  # e.g. "my-database-name"
-            query={
-                "unix_sock": "{}/{}/.s.PGSQL.5432".format(
-                    db_socket_dir,  # e.g. "/cloudsql"
-                    db_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
-            }
-        )
-    )
+    # Equivalent URL:
+    #sqlalchemy.engine.url.URL(
+        #    drivername="postgresql+pg8000",
+        #    username=db_user,  # e.g. "my-database-user"
+        #    password=db_pass,  # e.g. "my-database-password"
+        #    database=db_name,  # e.g. "my-database-name"
+        #    query={
+        #        "unix_sock": "{}/{}/.s.PGSQL.5432".format(
+        #            db_socket_dir,  # e.g. "/cloudsql"
+        #            db_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+        #    }
+        #)
+    url = "postgresql+pg8000://" + db_user + ":" + db_pass + "@35.230.182.58/" + db_name + "?unix_sock=" + db_socket_dir + "/" + db_connection_name+ "/.s.PGSQL.5432"
+    urlMySQL = "mysql://" + db_user + ":" + db_pass + "@35.245.6.29/" + db_name + "?unix_sock=" + db_socket_dir + "/" + db_connection_name
+    print(url)
+    pool = sqlalchemy.create_engine(url)
+    pool.dialect.description_encoding = None
     print(db_user)
     print(db_pass)
     print(db_name)
     print(db_connection_name)
+    print(db_socket_dir)
     return pool
     #unix_socket = '/cloudsql/{}'.format(db_connection_name)
                 #host='localhost',
@@ -61,8 +62,17 @@ def get_books():
     conn = open_connection()
     with conn.connect() as cursor:
         result = cursor.execute('SELECT * FROM books;')
-        books = cursor.fetchall()
-        if result > 0:
+        books = result.fetchall()
+        temp = []
+        print(result)
+        print(books)
+        for row in result:
+            print(row)
+            print(row['title'])
+            temp.append(row)
+
+        return flask.jsonify(temp)
+        if len(books) > 0:
             got_books = flask.jsonify(books)
         else:
             got_books = 'No Songs in DB'
@@ -82,7 +92,5 @@ def add_books(book):
             logging.debug(e)
             print(e)
             return e
-    conn.commit()
     logging.debug("made it passed committing")
     print("past committing")
-    conn.close()
